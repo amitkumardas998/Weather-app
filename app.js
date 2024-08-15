3c6bfc92db8a1926373fd62f8dc047ab8b673267
Amit Kumar Das <amitkumardas99689@gmail.com>
2024-08-15 20:06:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`;
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const weather = document.querySelector("#weather");
+
+const getWeather = async (city) => {
+    weather.innerHTML = <h2>Loading...</h2>;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        showWeather(data);
+    } catch (error) {
+        weather.innerHTML = <h2>City Not Found</h2>;
+    }
+};
+
+const showWeather = (data) => {
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4>${data.weather[0].main}</h4>
+        </div>
+    `
+};
+
+form.addEventListener("submit", function (event) {
+    getWeather(search.value);
+    event.preventDefault();
+});
